30c53b1205187a42a11fc03618b110bc77e4178f
Gerardo Luna <gerwi@CHIKOWISEONG15.>
2025-06-26 13:55:47 -0600

    Gerardo estuvo aqui

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
 
 @app.get("/")
 def inicio():
-    return {"mensaje": "Bienvenido a la Calculadora API\nUlises estuvo aqui"}
+    return {"mensaje": "Bienvenido a la Calculadora API\nUlises estuvo aqui\nGerardo estuvo aqui lml"}
 
 # Incluir rutas desde el router
 app.include_router(calculadora_router.router, prefix="/calculadora", tags=["Operaciones"])
